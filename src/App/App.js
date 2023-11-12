ec6d10be6aae7b8bc14ba8f7d49968f3be5049eb
Dan <drdkempson@live.com.au>
2023-11-12 11:33:46 +1000

    Transfer Intro Message into Component

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from '../NavBar/NavBar';
 import App_Footer from '../App-Footer/App-Footer';
+import Intro from '../Introduction/Intro';
 
 function App() {
   return (
@@ -8,13 +9,8 @@ function App() {
       <nav className='Nav-Bar'>
         <NavBar />
       </nav>
-      <header className="App-header">
-          <div className='Intro-container'>
-            <h2>Had some thoughts?</h2>
-            <p>Why not write them down?<br/>As you take notes, feel free to drag them to the different containers as you are done with those thoughts!</p>
-          </div>
-      </header>
       <main className='App-main'>
+        <Intro />
       </main>
       <App_Footer/>
     </div>
---
+++ src/Introduction/Intro.js
@@ -0,0 +1,15 @@
+import './Intro.css';
+
+
+export default function Intro() {
+    return (
+        <>
+        <header className="App-header">
+          <div className='Intro-container'>
+            <h2>Had some thoughts?</h2>
+            <p>Why not write them down?<br/>As you take notes, feel free to drag them to the different containers as you are done with those thoughts!</p>
+          </div>
+        </header>
+        </>
+    )
+}
