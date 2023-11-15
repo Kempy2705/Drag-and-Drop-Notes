41484cb007e4e21ee5827a8e734cbcd4f2c01076
Dan <drdkempson@live.com.au>
2023-11-15 16:13:17 +1000

    Create Notes and list temporary placeholders in the Notepad Divs

---
+++ src/Note/Note.js
@@ -0,0 +1,11 @@
+import './Note.css';
+
+export default function Note() {
+    return (
+        <>
+        <div className='note'>
+            <textarea className='text-field' placeholder='Write Something...'/>
+        </div>
+        </>
+    )
+}
--- src/Notepad/Notepad.js
+++ src/Notepad/Notepad.js
@@ -1,18 +1,21 @@
 import './Notepad.css';
-
+import Note from '../Note/Note';
 
 export default function NotePad() {
     return (
         <>
         <div className='notepad-container'>
             <div className='notepad'>
-                <h2 className='note-title'>To Do</h2>
+                <h2 className='notepad-title'>To Do</h2>
+                <Note />
             </div>
             <div className='notepad'>
-                <h2 className='note-title'>In Progress</h2>
+                <h2 className='notepad-title'>In Progress</h2>
+                <Note />
             </div>
             <div className='notepad'>
-                <h2 className='note-title'>Done</h2>
+                <h2 className='notepad-title'>Done</h2>
+                <Note />
             </div>
         </div>
         </>
