18f6322ece46f4f76059d8319c90e1fb26d7054f
Daniel Kempson <drdkempson@live.com.au>
2023-11-21 12:42:30 +1000

    Remove Uneccessary Dependencies

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,7 @@ import NavBar from '../NavBar/NavBar';
 import AppFooter from '../App-Footer/AppFooter.js';
 import Intro from '../Introduction/Intro';
 import NotePad from '../Notepad/Notepad.js';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import uuid from 'react-uuid';
 
 
